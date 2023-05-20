743d19d755afbf479cbbbff7944c43a2079cc8d0
GitSkrud <euaggeloszigas@gmail.com>
2023-05-21 00:23:37 +0300

    added modal

--- src/app/calendar-card/calendar-card.component.ts
+++ src/app/calendar-card/calendar-card.component.ts
@@ -1,46 +1,46 @@
-import { Component, Input, OnInit, ViewChild, ElementRef  } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { Day, Task } from '../models/card-item-model';
+import { ModalService } from '../modal.service';
+
 @Component({
   selector: 'app-calendar-card',
   templateUrl: './calendar-card.component.html',
   styleUrls: ['./calendar-card.component.css'],
 })
-export class CalendarCardComponent{
-  @Input() day:Day;
-  hiddenInput:boolean;  
+export class CalendarCardComponent {
+  @Input() day: Day;
   @ViewChild('newTaskInput') newTaskInput!: ElementRef;
+  @Output() taskAdded: EventEmitter<Task> = new EventEmitter<Task>();
 
-  
-  constructor(){
+  constructor(public modalService: ModalService) {
     this.day = {} as Day;
-    this.hiddenInput = true;
   }
 
-    addTask() {
-      this.hiddenInput = false;
-       setTimeout(() => {
+  openModal() {
+    this.modalService.openModal();
+    setTimeout(() => {
       this.newTaskInput.nativeElement.focus();
     }, 0);
   }
 
-  onEnterKeyPressed(){
-    if(this.newTaskInput.nativeElement.value!== ""){
-    const newTask:Task = new Task(this.newTaskInput.nativeElement.value, false);
-    this.day.tasks.push(newTask);
+  closeModal() {
+    this.modalService.closeModal();
+  }
+
+  addTaskToCard() {
+    if (this.newTaskInput.nativeElement.value !== '') {
+      const newTask: Task = new Task(this.newTaskInput.nativeElement.value, false);
+      this.day.tasks.push(newTask);
+      this.taskAdded.emit(newTask); // Emit the task added event
     }
-    this.hiddenInput = true;
-    this.newTaskInput.nativeElement.value = "";
+    this.newTaskInput.nativeElement.value = '';
   }
 
   deleteTask(task: Task) {
-    this.day.tasks = this.day.tasks.filter(t => t !== task);
-  
-    // localStorage.setItem(`${this.day.name}_tasks`, JSON.stringify(this.day.tasks));
+    this.day.tasks = this.day.tasks.filter((t) => t !== task);
   }
-  
 
   toggleTaskDone(task: any) {
     task.isChecked = !task.isChecked;
   }
-  
 }
